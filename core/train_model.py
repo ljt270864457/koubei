383588a78de8879e20c54e12667659567cbeb7a3
liujiatian <270864457@qq.com>
2020-03-11 10:22:02 +0800

    fix bug

--- core/train_model.py
+++ core/train_model.py
@@ -4,7 +4,6 @@ import pickle
 import numpy as np
 import pandas as pd
 from sklearn.metrics import mean_squared_error
-from sklearn.ensemble import RandomForestRegressor
 import xgboost
 
 import lightgbm as lgb
@@ -144,7 +143,6 @@ def predict_lgb(df_test_X, df_test_y_7_days):
             y_hat = np.round(model.predict(test_X) * 0.97).astype(int)
         else:
             y_hat = np.round(model.predict(test_X)).astype(int)
-        # y_hat = np.round(model.predict(test_X)).astype(int)
         result.append(y_hat.reshape(-1, 1))
         rmse_list.append(np.sqrt(mean_squared_error(test_y, y_hat)))
 
@@ -162,59 +160,6 @@ def predict_lgb(df_test_X, df_test_y_7_days):
     print(f'loss:{round(loss,6)},rmse:{round(rmse,6)}')
 
 
-# def train_model_rf(df_train_X, df_train_y_7_days, df_test_X, df_test_y_7days):
-#     train_X = df_train_X.values
-#     model = RandomForestRegressor(n_estimators=100, max_depth=10, oob_score=True, min_samples_split=16,
-#                                   min_samples_leaf=8, max_leaf_nodes=32, max_features=0.85, )
-#
-#     for i in range(7):
-#         print(f'rf 训练第{i+1}个模型')
-#         train_y = df_train_y_7_days.iloc[:, i].values
-#
-#         model.fit(train_X, train_y)
-#         pickle.dump(model, open(f'{MODEL_DIR}/rf_day{i}.pkl', 'wb'))
-
-
-# def predict_rf(df_test_X, df_test_y_7_days):
-#     '''
-#     使用xgb模型进行预测
-#     :param df_test_X:
-#     :param df_test_y_7_days:
-#     :return:
-#     '''
-#     result = []
-#     rmse_list = []
-#
-#     test_X = df_test_X.values
-#
-#     for i in range(7):
-#         print(f'预测第{i+1}天...')
-#         path = f'{MODEL_DIR}/rf_day{i}.pkl'
-#         model = pickle.load(open(path, 'rb'))
-#         test_y = df_test_y_7_days.iloc[:, i].values
-#
-#         # # 通过误差分析 发现周末的销量存在较大的误差，在实际预测的时候加上0.95的策略参数
-#         if i in (4, 5):
-#             y_hat = np.round(model.predict(test_X) * 0.97).astype(int)
-#         else:
-#             y_hat = np.round(model.predict(test_X)).astype(int)
-#         result.append(y_hat.reshape(-1, 1))
-#         rmse_list.append(np.sqrt(mean_squared_error(test_y, y_hat)))
-#
-#     print('计算评价指标')
-#     predict_matrix = np.hstack(result)
-#     true_matrix = df_test_y_7_days.values
-#     df_predict = pd.DataFrame(predict_matrix)
-#     df_true = pd.DataFrame(true_matrix)
-#     df_predict.to_csv(f'{DATA_DIR}/prediction/xgb_predict.csv', index=False, header=False)
-#     df_true.to_csv(f'{DATA_DIR}/prediction/xgb_true.csv', index=False, header=False)
-#
-#     loss = np.sum(np.abs((true_matrix - predict_matrix) / (true_matrix + predict_matrix))) / true_matrix.shape[0] / \
-#            true_matrix.shape[1]
-#     rmse = np.mean(rmse_list)
-#     print(f'loss:{round(loss,6)},rmse:{round(rmse,6)}')
-
-
 if __name__ == '__main__':
     df_train_X, df_train_y_7_days, df_test_X, df_test_y_7days = load_data()
     # train_model_xgb(df_train_X, df_train_y_7_days, df_test_X, df_test_y_7days)
@@ -222,6 +167,3 @@ if __name__ == '__main__':
 
     # train_model_lgb(df_train_X, df_train_y_7_days, df_test_X, df_test_y_7days)
     predict_lgb(df_test_X, df_test_y_7days)
-
-    # train_model_rf(df_train_X, df_train_y_7_days, df_test_X, df_test_y_7days)
-    # predict_rf(df_test_X, df_test_y_7days)
